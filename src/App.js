a988ea5e796374d07c66a4cb5b47e51ebb22a3cb
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-08-12 12:36:30 -0700

    Completed the prodct context portion. Working on the cart part at the moment.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import {ProductContext} from "./contexts/ProductContext.js";
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -19,17 +22,16 @@ function App() {
 		<div className="App">
 			<Navigation cart={cart} />
 
+			<ProductContext.Provider value={{products, addItem}}>
+	
+				<Route
+					exact
+					path="/"
+					component={Products}
+				/>
+			</ProductContext.Provider>
+
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
 
 			<Route
 				path="/cart"
